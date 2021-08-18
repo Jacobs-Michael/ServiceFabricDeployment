91bc3a642faebd3e5bb7f39cd72e2c726b3f28a4
Michael Jacobs <michael.jacobs@aquasec.com>
2021-08-18 12:58:59 -0400

    Added -UseBasicParsing, retry on fail, and setting of local firewall rules

--- ServiceFabricInstall.ps1
+++ ServiceFabricInstall.ps1
@@ -1,6 +1,27 @@
-$User = <USER>
-$PWord = ConvertTo-SecureString -String “<Password>“ -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-$source = 'https://download.aquasec.com/windows-enforcer/6.2.0/AquaAgentWindowsInstaller.6.2.21216.msi'
-$destination = 'aqua_installer.msi'
-Invoke-WebRequest -Uri $source -OutFile $destination -Credential $credential
+function Download-Executable ($count){
+    try{
+        New-NetFirewallRule -DisplayName "Allow Outbound Port 8443" -Direction Outbound -LocalPort 8443 -Protocol RPC -Action Allow
+        New-NetFirewallRule -DisplayName "Allow Inbound Port 8443" -Direction Inbound -LocalPort 8443 -Protocol TCP -Action Allow
+
+        $User = "<AQUA_USER>"
+        $PWord = ConvertTo-SecureString -String "<AQUA_PASSWORD>" -AsPlainText -Force
+        $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+        $source = 'https://download.aquasec.com/windows-enforcer/6.2.0/AquaAgentWindowsInstaller.6.2.21216.msi'
+        $destination = 'AquaAgentWindowsInstaller.msi'
+        Invoke-WebRequest -UseBasicParsing -Uri $source -OutFile $destination -Credential $Credential
+
+        msiexec /I AquaAgentWindowsInstaller.msi AQUA_TOKEN=<TOKEN_FROM_UI> AQUA_SERVER=<GATEWAY_ADDRESS>:8443 /qn
+
+        Write-Host "Completed Successfully"
+    }
+    catch{
+	$count = $count + 1
+	if ( $count -lt 4 ){
+		Download-Executable $count
+	}
+        Write-Host "Error Ocurred"
+        Write-Host $_
+    }   
+}
+
+Download-Executable 1
