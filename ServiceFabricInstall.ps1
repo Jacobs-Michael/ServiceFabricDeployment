8e49907b488fa2f363ecfbb195b4d99e3e1d1810
Michael Jacobs <michael.jacobs@aquasec.com>
2021-08-18 17:06:05 -0400

    Cahged description of firewall rules

--- ServiceFabricInstall.ps1
+++ ServiceFabricInstall.ps1
@@ -1,8 +1,8 @@
 function Download-Executable ($count){
     try{
         <# Port must be changed to 8443 for an on-premise installation #>
-        New-NetFirewallRule -DisplayName "Allow Outbound Port 8443" -Direction Outbound -LocalPort 443 -Protocol RPC -Action Allow
-        New-NetFirewallRule -DisplayName "Allow Inbound Port 8443" -Direction Inbound -LocalPort 443 -Protocol TCP -Action Allow
+        New-NetFirewallRule -DisplayName "Allow Outbound Port 443" -Direction Outbound -LocalPort 443 -Protocol RPC -Action Allow
+        New-NetFirewallRule -DisplayName "Allow Inbound Port 443" -Direction Inbound -LocalPort 443 -Protocol TCP -Action Allow
 
         $User = "<AQUA_USER>"
         $PWord = ConvertTo-SecureString -String "<AQUA_PASSWORD>" -AsPlainText -Force
